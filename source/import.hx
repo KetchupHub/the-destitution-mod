a7c736dbe0528bfdc3524e06c960a91f8c8fe22f
Cynda <65635932+CyndaquilDAC@users.noreply.github.com>
2024-09-16 18:23:38 -0500

    ffff

--- source/import.hx
+++ source/import.hx
@@ -1,7 +1,22 @@
 import Paths;
 
+#if !macro
+using Lambda;
+using StringTools;
+using thx.Arrays;
+
 #if DEVELOPERBUILD
 import util.Perf;
 #end
 
-using StringTools;
+using util.MemoryUtil;
+
+using util.tools.ArraySortTools;
+using util.tools.ArrayTools;
+using util.tools.FloatTools;
+using util.tools.Int64Tools;
+using util.tools.IntTools;
+using util.tools.IteratorTools;
+using util.tools.MapTools;
+using util.tools.StringTools;
+#end
